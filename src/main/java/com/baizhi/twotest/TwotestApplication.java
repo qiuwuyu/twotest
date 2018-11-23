df963c958c7ede6a8373ad3eb27c074ced45fc94
qiuyu <junbuyu.com>
2018-11-23 19:31:27 +0800

    v1.0

---
+++ src/main/java/com/baizhi/twotest/TwotestApplication.java
@@ -0,0 +1,12 @@
+package com.baizhi.twotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwotestApplication.class, args);
+    }
+}
